ac00b9c59ec44927cbd888f4e3f4d627da847b05
Maxim Fomov <maksimfomov26@gmail.com>
2023-07-25 17:32:26 +0300

    init: Project created and configured

---
+++ data/src/main/java/com/fomov/itbootcamptesttask/data/App.java
@@ -0,0 +1,13 @@
+package com.fomov.itbootcamptesttask.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web/src/main/java/com/fomov/itbootcamptesttask/web/ItBootcampTestTaskApplication.java
@@ -0,0 +1,14 @@
+package com.fomov.itbootcamptesttask.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBootcampTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBootcampTestTaskApplication.class, args);
+	}
+
+}
+
