41344db2c1f6a2a068f4bbcacad958c5e14a75ca
Maxim Fomov <maksimfomov26@gmail.com>
2023-07-26 14:50:40 +0300

    feat: Added sorting by email

--- core/src/main/java/com/fomov/itbootcamptesttask/core/service/impl/UserServiceImpl.java
+++ core/src/main/java/com/fomov/itbootcamptesttask/core/service/impl/UserServiceImpl.java
@@ -22,6 +22,6 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> getAllUsers() {
-		return userRepository.findAll();
+		return userRepository.findAllByOrderByEmailAsc();
 	}
 }
