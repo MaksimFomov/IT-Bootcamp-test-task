0c385950697e7909467eae30ead432a54a1dc3c1
Maxim Fomov <maksimfomov26@gmail.com>
2023-07-26 10:29:08 +0300

    feat: Added UserRepository

---
+++ data/src/main/java/com/fomov/itbootcamptesttask/data/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fomov.itbootcamptesttask.data.repository;
+
+import com.fomov.itbootcamptesttask.data.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
